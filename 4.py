23f472e7f080de468f888bbc3a9fd85e126de00f
Diego <diego@diego.cat>
2022-12-04 17:16:03 +0100

    Fourth puzzle (part one)

--- 4.py
+++ 4.py
@@ -4,3 +4,20 @@ Puzzle 4
 """
 
 from helpers import getInput
+
+def getRange(elf: list) -> set:
+    firstSector = int(elf.split("-")[0])
+    lastSector = int(elf.split("-")[1])
+
+    return set(range(firstSector, lastSector+1))
+
+overlaps = 0
+
+for section in getInput(4):
+    firstElfRange = getRange(section.split(",")[0])
+    secondElfRange = getRange(section.split(",")[1])
+
+    if firstElfRange.issubset(secondElfRange) or secondElfRange.issubset(firstElfRange):
+        overlaps += 1
+
+print(overlaps)
