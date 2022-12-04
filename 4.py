2b96e612f6cad718a6915ecbd57ea84c1646df1d
Diego <diego@diego.cat>
2022-12-04 15:00:35 +0100

    Updated helper

---
+++ 4.py
@@ -0,0 +1,6 @@
+"""
+Advent of Code 2022
+Puzzle 4
+"""
+
+from helpers import getInput
--- helpers.py
+++ helpers.py
@@ -3,11 +3,6 @@ Advent of Code 2022
 Helpers
 """
 
-def getInput(id: int):
-    lines = []
-
-    with open(f"./inputs/{id}.txt", "r") as file:
-        for line in file:
-            lines.append(line.strip())
-
-    return lines
+def getInput(id: int) -> list:
+    with open(f"./inputs/{id}.txt") as file:
+        return list(map(lambda line: line.strip(), file))
